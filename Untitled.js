37719c75220052a78dd9ad3c6a340e0d3594ac3a
ViviDiElle <dileonardov@yahoo.com>
2023-04-03 13:34:17 +0200

    done

--- Untitled.js
+++ Untitled.js
@@ -23,6 +23,7 @@ function fetchData(){
 
 }
 
+
 function changeImg(){
     const main = document.querySelector('main')
         const svgs = main.querySelectorAll('svg')
@@ -44,11 +45,59 @@ function insertImg(){
     )
 }
 
-/*
-function insertLandscape(){
-    let imgs = document.querySelectorAll('img')
-    imgs.forEach((img, i) => {
-        img.setAttribute('src', risultato.photos[i].src.landscape)
-    })
-}*/
 
+
+function hideBtn() {
+    let editBtn = document.querySelectorAll('.btn-outline-secondary:nth-of-type(2)')
+    editBtn.forEach((elem)=>{
+        elem.innerHTML = `Hide`
+        elem.addEventListener('click', function(e){
+            let card = e.target.parentNode.parentNode.parentNode.parentNode
+            card.remove()
+        })
+
+    })
+}
+
+
+window.onload = function() {
+    hideBtn()
+    addInput()
+    insertKeyword()
+}
+
+function addInput() {
+    let jumbotron = document.querySelector ('#jumbo')
+    let input = document.createElement('input')
+    input.setAttribute('type', 'text')
+    input.setAttribute('placeholder', 'inserisci parola chiave')
+    input.setAttribute('name', 'my-input')
+    input.setAttribute('id', 'input-key')
+    jumbotron.appendChild(input)
+}
+
+function insertKeyword() {
+    let keywordd = document.getElementById('input-key').value
+    if (!keywordd) {
+        alert('Inserisci keyword')
+    }
+    else {
+    const apiUrl3 =`https://api.pexels.com/v1/search?query=${keywordd}`
+    fetch(apiUrl3, {
+        headers: {
+            Authorization: apiKey
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        risultato = data;
+        console.log(risultato);
+        
+        //Sostituzione IMG:
+        changeImg()
+        
+        //INSERISCI IMMAGINI
+       insertImg()
+    })
+    }
+}
